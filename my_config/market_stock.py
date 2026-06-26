6991ff0afe7798c3e7930e6fa846306c590516e8
francaustine-lang <francaustine@gmail.com>
2026-06-26 11:52:43 +0100

    feat(scafolding): updated market stock

--- my_config/market_stock.py
+++ my_config/market_stock.py
@@ -2,5 +2,17 @@ balance = int(input("Your Total Cash: "))
 orange = 500
 mangoe = 250
 grape = 450
+select = input("Select your item ('orange', 'mangoe', 'grape'): ")
+if select == orange:
+    select = orange
+elif select == mangoe:
+    select = mangoe
+elif select == grape:
+    select = grape
+
 while True:
-    if 
+    if balance == 0:
+        break
+    else:
+        balance = balance - select
+print(balance)
