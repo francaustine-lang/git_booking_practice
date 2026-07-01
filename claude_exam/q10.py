6729b3dfd6168967995b5c98502d34ee6673703a
francaustine-lang <francaustine@gmail.com>
2026-07-01 13:30:50 +0100

    (scafolding): updating works done

--- claude_exam/q10.py
+++ claude_exam/q10.py
@@ -1,1 +1,18 @@
 #Q10. Use a loop to count how many vowels (a, e, i, o, u) are in a given string, e.g. "Francaustine".
+name = "Francaustine"
+vowel = "aeiou"
+count = 0
+for i in name:
+    for j in vowel:
+        if i == j:
+            count +=1
+print(count)
+
+#Q10. Use a loop to count how many vowels (a, e, i, o, u) are in a given string, e.g. "Francaustine".
+name = "Thunder waec"
+vowel = "aeiou"
+count = 0
+for i in name:
+    if i in vowel:
+        count +=1
+print(count)
