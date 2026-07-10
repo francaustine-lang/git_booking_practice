a9166785158129025578caec68f9c166eefff82c
francaustine-lang <francaustine@gmail.com>
2026-07-10 11:55:52 +0100

    modified the answer

--- claude_exam_again/q6.py
+++ claude_exam_again/q6.py
@@ -1,11 +1,7 @@
 #Q6. Write a loop that prints numbers from 1 to 50, 
 # but skips any number divisible by 3 or 5 (use continue).
-arc = 0
-bet = 1
 for numb in range(1, 51):
     if numb % 3 == 0 or numb % 5 == 0:
-        arc.append(numb)
-    else:
-        bet.append(numb)
-print(arc)
+        continue
+    print(numb, end='\t')
     
