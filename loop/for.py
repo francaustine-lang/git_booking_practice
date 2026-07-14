28b60fd1a2a9c147d1b3deeeef28eea59147d39d
francaustine-lang <francaustine@gmail.com>
2026-07-14 11:23:11 +0100

    feat(remote): commiting my works of 12/7/26

--- loop/for.py
+++
@@ -1,9 +0,0 @@
-#determine whether it is a positive or negative number
-
-x = int(input("Type any Number: "))
-while True:
-    if x > 0:
-        message = "is Positive"
-    else:
-        message = "is Negative"
-print(x, message)
